d322b3901546ccccf6831f87237653aab3c2e0b0
YogyerekDEV <mszebi2008@gmail.com>
2026-04-09 13:40:49 +0200

    dev(W-01): add RegisterDto

---
+++ Webshop.api/DTOs/RegisterDto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Webshop.api.DTOs;
+
+public class RegisterDto
+{
+    [Required]
+    [MinLength(3)]
+    public string Username { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(6)]
+    public string Password { get; set; } = string.Empty;
+}
